be438fc1ec64b45c1e5e1e7b6af7b1166b8b6fa4
Unknown <slyguggi@gmail.com>
2019-03-14 18:03:54 +0100

    upload from laptop

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,165 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	[SerializeField] private float fadeInTimescaleTime = 0.1f;
+	public static GameManager Instance { get; private set; }
+	private Coroutine _timeScaleCoroutine;
+	private PlayerController _player;
+	public PlayerController Player => _player;
+	private bool _fadeOutToBlack = false;
+	private bool _isQuitting;
+
+	public bool FadeOutToBlack
+	{
+		get => _fadeOutToBlack;
+		set => _fadeOutToBlack = value;
+	}
+
+	private void OnEnable()
+	{
+		SceneManager.sceneLoaded += OnLevelFinishedLoadingScene;
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+
+	private void OnDisable()
+	{
+		SceneManager.sceneLoaded -= OnLevelFinishedLoadingScene;
+	}
+
+	//this function is activated every time a scene is loaded
+	private void OnLevelFinishedLoadingScene(Scene scene, LoadSceneMode mode)
+	{
+		Setup();
+		if (_fadeOutToBlack)
+		{
+			//UIManager.FadeToBlack(false);
+			_fadeOutToBlack = false;
+		}
+	}
+
+	private void Setup()
+	{
+		//alternative way to get elements. cons : if there is no element with such tag it creates an error
+		//_player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+		_player = FindObjectOfType<PlayerController>();
+	}
+
+	private void Awake()
+	{
+		if (Instance != null && Instance != this)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+
+		Setup();
+	}
+
+	public void LoadLevel(string nameLevel)
+	{
+		SceneManager.LoadScene(nameLevel);
+	}
+
+
+	public void LoadLevelFadeInAndOut(string nameLevel)
+	{
+		//UIManager.FadeToBlack(true);
+		_fadeOutToBlack = true;
+		StartCoroutine(LoadingLevel(nameLevel));
+	}
+
+	public void LoadLevel(string nameLevel, bool fadeInToBlack, bool fadeOutToBlack)
+	{
+		if (fadeInToBlack)
+		{
+			//_player.StopMoving();
+			//_uiManager.FadeToBlack(true);
+			StartCoroutine(LoadingLevel(nameLevel));
+		}
+		else
+		{
+			LoadLevel(nameLevel);
+		}
+
+		_fadeOutToBlack = fadeOutToBlack;
+	}
+
+	private IEnumerator LoadingLevel(string nameLevel)
+	{
+		/*while (UIManager.IsFadingToBlack)
+		{
+			yield return null;
+		}*/
+		yield return null;
+		LoadLevel(nameLevel);
+	}
+
+	public void ChangeTimeScale(float timeScale)
+	{
+		if (_timeScaleCoroutine != null)
+		{
+			StopCoroutine(_timeScaleCoroutine);
+		}
+
+		if (fadeInTimescaleTime.CompareTo(0) > 0)
+		{
+			_timeScaleCoroutine = StartCoroutine(ChangingTimeScale(timeScale));
+		}
+		else
+		{
+			Time.timeScale = timeScale;
+		}
+	}
+
+	private IEnumerator ChangingTimeScale(float timeScale)
+	{
+		float timer = 0.0f;
+		float initTimeScale = Time.timeScale;
+		while (timer < fadeInTimescaleTime)
+		{
+			timer += Time.unscaledDeltaTime;
+			Time.timeScale = Mathf.Lerp(initTimeScale, timeScale, timer / fadeInTimescaleTime);
+			yield return null;
+		}
+	}
+
+	private IEnumerator QuittingGame()
+	{
+		/*while (UIManager.IsFadingToBlack)
+		{
+			yield return null;
+		}*/
+		yield return null;
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+
+	public void QuitGame()
+	{
+		if (!_isQuitting)
+		{
+			_isQuitting = true;
+			//_uiManager.FadeToBlack(true);
+			StartCoroutine(QuittingGame());
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float moveSpeed = 5.0f;
+	[SerializeField] private float sprintSpeed = 10.0f;
+	[SerializeField] private float rotationSpeed = 10.0f;
+	[SerializeField] private float angleLimit = 80.0f;
+	[SerializeField] private Transform myCamera = null;
+
+	private float _horizontalInput;
+	private float _verticalInput;
+	private Rigidbody _myRigidBody;
+	private bool _canSprint = true;
+	private float _actualSpeed;
+
+	private void Start()
+	{
+		_myRigidBody = GetComponent<Rigidbody>();
+		_actualSpeed = moveSpeed;
+	}
+
+	private void Update()
+	{
+		_horizontalInput = Input.GetAxis("Horizontal");
+		_verticalInput = Input.GetAxis("Vertical");
+
+		if (Input.GetButtonDown("Fire1") && _canSprint)
+		{
+			_actualSpeed = sprintSpeed;
+		}
+		else if (Input.GetButtonUp("Fire1"))
+		{
+			_actualSpeed = moveSpeed;
+		}
+		
+		
+		//calculate the angle between the player and the laser
+		Vector2 diff = transform.right*Input.GetAxis("Mouse X");
+		Quaternion focusAngle = Quaternion.Euler(0f, 0f, Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg - 90);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, focusAngle, rotationSpeed * Time.deltaTime);
+
+	}
+
+	private void FixedUpdate()
+	{
+		_myRigidBody.velocity = _actualSpeed * (transform.right.normalized * _horizontalInput +
+												transform.forward.normalized * _verticalInput);
+	}
+}
