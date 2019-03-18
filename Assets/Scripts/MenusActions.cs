1ba15bac543549784b5ee4f6cc96706302f448b3
Simon <slyguggi@gmail.com>
2019-03-18 17:09:49 +0100

    added working main menu

---
+++ Assets/Scripts/MenusActions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenusActions : MonoBehaviour
+{
+	public void LoadLevelFadeInAndOut(string nameLevel)
+	{
+		GameManager.Instance.LoadLevelFadeInAndOut(nameLevel);
+	}
+
+	public void QuitGame()
+	{
+		GameManager.Instance.QuitGame();
+	}
+
+	public void ChangeTimeScale(float timeScale)
+	{
+		GameManager.Instance.ChangeTimeScale(timeScale);
+	}
+}
