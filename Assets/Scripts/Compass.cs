81ccb16d5b4d03c117544cf3a9e5cdcddaac291e
Simon <slyguggi@gmail.com>
2019-03-18 16:39:58 +0100

    reworked compass

--- Assets/Scripts/Compass.cs
+++ Assets/Scripts/Compass.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Compass : MonoBehaviour
 {
 	[SerializeField] private Vector3 compassAxis = Vector3.forward;
+
 	private void Update()
 	{
 		transform.forward = compassAxis;
