0574d729caa805339b8a7fc48dae0ae43b8246ce
Simon <slyguggi@gmail.com>
2019-03-20 00:34:17 +0100

    added popup message, to test

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,13 +9,21 @@ public class UIManager : MonoBehaviour
 {
 	[SerializeField] private float fadingToBlackTime = 0.5f;
 	[SerializeField] private Image blackPanel = null;
-	[SerializeField] private GameObject dialoguePanel = null;
-	[SerializeField] private TextMeshProUGUI textDisplayed = null;
-	private Coroutine _currentDialogue;
+	[SerializeField] private GameObject dialoguePanelBottom = null;
+	[SerializeField] private TextMeshProUGUI textDisplayedBottom = null;
+	[SerializeField] private GameObject dialoguePanelTop = null;
+	[SerializeField] private TextMeshProUGUI textDisplayedTop = null;
+	private Coroutine _currentDialogueTop;
+	private Coroutine _currentDialogueBottom;
 	private Message _currentMessage;
 	private bool _isFadingToBlack;
 	public bool IsFadingToBlack => _isFadingToBlack;
-	
+
+	public enum DialoguePosition
+	{
+		Top,
+		Bottom
+	}
 
 	public void FadeToBlack(bool value)
 	{
@@ -42,10 +50,31 @@ public class UIManager : MonoBehaviour
 
 	public void PrintMessage(Message message)
 	{
-		dialoguePanel.SetActive(true);
-		if (_currentDialogue != null)
+		PrintMessage(message, DialoguePosition.Bottom);
+	}
+
+	public void PrintMessage(Message message, DialoguePosition dialoguePosition)
+	{
+		GameObject dialoguePanel;
+		TextMeshProUGUI textDisplayed;
+		Coroutine tempCoroutine;
+		if (dialoguePosition == DialoguePosition.Top)
 		{
-			StopCoroutine(_currentDialogue);
+			dialoguePanel = dialoguePanelTop;
+			textDisplayed = textDisplayedTop;
+			tempCoroutine = _currentDialogueTop;
+		}
+		else
+		{
+			dialoguePanel = dialoguePanelBottom;
+			textDisplayed = textDisplayedBottom;
+			tempCoroutine = _currentDialogueBottom;
+		}
+
+		dialoguePanel.SetActive(true);
+		if (tempCoroutine != null)
+		{
+			StopCoroutine(tempCoroutine);
 		}
 
 		_currentMessage = message;
@@ -53,21 +82,46 @@ public class UIManager : MonoBehaviour
 		textDisplayed.color = _currentMessage.color;
 		if (message.timeBetweenLetters.CompareTo(0) != 0)
 		{
-			_currentDialogue = StartCoroutine(PrintLetterByLetter());
+			tempCoroutine = StartCoroutine(PrintLetterByLetter(dialoguePosition));
 		}
 		else
 		{
-			PrintAll();
+			PrintAll(dialoguePosition);
 		}
 	}
 
 	public void CloseMessage()
 	{
+		CloseMessage(DialoguePosition.Bottom);
+	}
+
+	public void CloseMessage(DialoguePosition dialoguePosition)
+	{
+		GameObject dialoguePanel;
+		if (dialoguePosition == DialoguePosition.Top)
+		{
+			dialoguePanel = dialoguePanelTop;
+		}
+		else
+		{
+			dialoguePanel = dialoguePanelBottom;
+		}
+
 		dialoguePanel.SetActive(false);
 	}
 
-	private IEnumerator PrintLetterByLetter()
+	private IEnumerator PrintLetterByLetter(DialoguePosition dialoguePosition)
 	{
+		TextMeshProUGUI textDisplayed;
+		if (dialoguePosition == DialoguePosition.Top)
+		{
+			textDisplayed = textDisplayedTop;
+		}
+		else
+		{
+			textDisplayed = textDisplayedBottom;
+		}
+
 		textDisplayed.text = "";
 		for (int i = 0; i < _currentMessage.text.Length; i++)
 		{
@@ -76,8 +130,18 @@ public class UIManager : MonoBehaviour
 		}
 	}
 
-	private void PrintAll()
+	private void PrintAll(DialoguePosition dialoguePosition)
 	{
+		TextMeshProUGUI textDisplayed;
+		if (dialoguePosition == DialoguePosition.Top)
+		{
+			textDisplayed = textDisplayedTop;
+		}
+		else
+		{
+			textDisplayed = textDisplayedBottom;
+		}
+
 		textDisplayed.text = _currentMessage.text;
 	}
 }
